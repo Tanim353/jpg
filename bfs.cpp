730e759d308944f4ca92b5ddde6fc81f7c3d5d01
Tanim <156685129+Tanim353@users.noreply.github.com>
2024-10-01 01:23:12 +0600

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,80 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ld long double
-#define ff first
-#define sc second
-#define mp make_pair
-#define pb push_back
-#define pp pop_back
-#define nn "\n"
 
-using namespace std;
-vector<ll> v[1000];
-ll visit[1000];
-
-ll d[1000] = {0};
-
-ll pre[1000] = {0};
-
-ll n, l;
-
-void ss(int a)
+const int N = 10000;
+bool visit[N] = {false};
+vector<int> g[N];
+void bfs(int a)
 {
-    if (pre[a] == 0)
-    {
-        cout << pre[a] << " ";
-    }
-    else
-    {
-        ss(a - 1);
-        cout << pre[a] << " ";
-    }
-}
-void bfs(ll x)
-{
-    queue<ll> q;
-    q.push(x);
-    visit[x] = 1;
+    queue<int> q;
+    q.push(a);
+    visit[a] = true;
     while (!q.empty())
     {
-        ll current = q.front();
-        cout << current << " ";
+        int k = q.front();
         q.pop();
-        for (ll i = 0; i < v[current].size(); i++)
+        cout << k << " ";
+        for (auto it : g[k])
         {
-            ll next = v[current][i];
-            if (visit[next] == 0)
+            if (!visit[it])
             {
-                visit[next] = 1;
-                d[next] = d[current] + 1;
-                pre[next] = current;
-                q.push(next);
+                q.push(it);
+                visit[it] = true;
             }
         }
     }
 }
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> n >> l;
-    for (ll i = 0; i < l; i++)
+    int n, e, x, y, src;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++)
     {
-        ll x, y;
         cin >> x >> y;
-        v[x].pb(y);
+        g[x].push_back(y);
+        g[y].push_back(x);
     }
-    bfs(0);
-    cout << endl;
-    for (ll i = 0; i < n; i++)
-    {
-        cout << i << " Weight: " << d[i] << " Path: " << endl;
-        ss(i);
-        cout << i << endl;
-    }
-
-    return 0;
+    cin >> src;
+    bfs(src);
 }
